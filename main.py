6e515f20983ded0fdfe1dd37eec5efb8a5b531d9
archieLa <arturm.lach@gmail.com>
2018-05-12 00:13:41 -0500

    Fixed __init__ bugs and variables names

--- main.py
+++ main.py
@@ -3,33 +3,33 @@ import digitalio
 import time
 
 class PresenceSensor:
-    def _init_(self):
-        self.objectDetected = digitalio.DigitalInOut(board.D11)
-        self.objectDetected.direction = digitalio.Direction.INPUT
+    def __init__(self):
+        self.presenceSignal = digitalio.DigitalInOut(board.D4)
+        self.presenceSignal.direction = digitalio.Direction.INPUT
     def checkPresence(self):
-        return self.objectDetected.value
+        return (!self.presenceSignal.value) #Reverse sensor logic to be more intuitive
                 
 class SmileySpeakerOutput:
-    def _init_(self):
-        self.speakerOn = digitalio.DigitalInOut(board.D12)
+    def __init__(self):
+        self.speakerOn = digitalio.DigitalInOut(board.D3)
         self.speakerOn.direction = digitalio.Direction.OUTPUT
-        self.smileyOn = digitalio.DigitalInOut(board.D13)
+        self.smileyOn = digitalio.DigitalInOut(board.D2)
         self.smileyOn.direction = digitalio.Direction.OUTPUT
-        self.onDelay = 0.5
+        self.onDelay = 1
     def trigger(self):
-        speakerOn.value = True
-        smileyOn.value = True
-        time.sleep(self.onDelay)
-        speakerOn.value = False
-        smileyOn.value = False
+        self.speakerOn.value = True
+        self.smileyOn.value = True
+        self.time.sleep(self.onDelay)
+        self.speakerOn.value = False
+        self.smileyOn.value = False
 
 class CubeWelcomer:
-    def _init_(self):
+    def __init__(self):
         self.smiley_speaker = SmileySpeakerOutput()
         self.object_in_view = False
     def toGreetOrNotToGreet(self, objectPresent):
         if (self.object_in_view == False and
-        objectPresent == True):
+        objectPresent == True): 
             SmileySpeakerOutput.trigger()
             self.object_in_view = True
         elif (self.object_in_view == True and
@@ -40,6 +40,6 @@ cubeWelcomer = CubeWelcomer()
 presenceSensor = PresenceSensor()
 
 while True:
-    cubeWelcomer.toGreetOrNotToGreet(presenceSensor.checkPresence)
-    time.sleep(0.25)
+    cubeWelcomer.toGreetOrNotToGreet(presenceSensor.checkPresence())   
+    time.sleep(0.5)
     
